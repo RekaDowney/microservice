7a194fbb24deb4c98edb00452849f83ded4c65a6
钟俊滨 <rekadowney@163.com>
2018-07-20 23:32:29 +0800

    改造服务提供方8081，将其注册到eureka中

--- provider8001/src/main/java/me/junbin/microservice/Provider8001Application.java
+++ provider8001/src/main/java/me/junbin/microservice/Provider8001Application.java
@@ -2,6 +2,7 @@ package me.junbin.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : Zhong Junbin
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @createDate : 2018/7/20 9:34
  * @description :
  */
+// 将服务作为 Eureka 客户端，借此向 Eureka 服务端注册自身
+@EnableEurekaClient
 @SpringBootApplication
 public class Provider8001Application {
 
