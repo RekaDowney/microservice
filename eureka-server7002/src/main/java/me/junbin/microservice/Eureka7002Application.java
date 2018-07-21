f1c51fabaa56e1e319cf47187f320eb64e40d50e
钟俊滨 <rekadowney@163.com>
2018-07-21 23:26:25 +0800

    介绍EurekaServer集群

---
+++ eureka-server7002/src/main/java/me/junbin/microservice/Eureka7002Application.java
@@ -0,0 +1,22 @@
+package me.junbin.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : Zhong Junbin
+ * @email : <a href="mailto:rekadowney@gmail.com">发送邮件</a>
+ * @createDate : 2018/7/20 22:50
+ * @description :
+ */
+// 启动 Eureka Server，接受其他微服务注册进来，启动后访问 http://${host}:${server.port} 如果看到 Eureka Server 页面则表示配置启动成功
+@EnableEurekaServer
+@SpringBootApplication
+public class Eureka7002Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Eureka7002Application.class, args);
+    }
+
+}
